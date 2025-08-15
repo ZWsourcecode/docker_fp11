fd0501d15bd825793434884f2391accb7b61f53d
ZWsourcecode <wuzhendong52@gmail.com>
2025-08-15 13:51:13 +0200

    update scripts for input settings

--- flexpart11set_global_14Croutine.sh
+++ flexpart11set_global_14Croutine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-### create pathnames, RELEASES, COMMAND file for flexpart
+### create pathnames, RELEASES, COMMAND, OUTGRID, OUTGRID_NEST, AGECLASSES  file for flexpart
 ### script should be in parent folder of options
-### run : bash flexpartset.sh 
+### run : bash flexpart11set.sh 11 inputs
 ### zhendong.wu@nateko.lu.se
 
 HOME_PATH=$(pwd)
@@ -9,7 +9,7 @@ OPTION_PATH=$HOME_PATH/options
 
 LIFETIME=10 # in days
 
-echo ------ make pathnames, RELEASES, COMMAND, OUTGRID, OUTGRID_NEST file ------
+echo ------ make pathnames, RELEASES, COMMAND, OUTGRID, OUTGRID_NEST, AGECLASSES file ------
 echo
 
 # ======================================
@@ -99,40 +99,20 @@ EOM
 start_release=$(date -u -d "$start")
 end_release=$(date -u -d "$end -$step hours")
 
-# i=-$step
-# while [ "$thedate" != "$end_release" ]
-# do
-#  	thedate=$(date -u -d "$start +$i hours")
-# # 	echo $thedate
-# 	IDATE1=$(date -u -d "$start +$i hours" +%Y%m%d)
-# 	ITIME1=$(date -u -d "$start +$i hours" +%H%M%S)
-# # 	echo $IDATE1
-# # 	echo $ITIME1
-# 	
-# 	i=$(( i + $step ))
-# 	
-# 	IDATE2=$(date -u -d "$start +$i hours" +%Y%m%d)
-# 	ITIME2=$(date -u -d "$start +$i hours" +%H%M%S)
-# # 	echo $IDATE2
-# # 	echo $ITIME2
-
 i=0
+releaseno=0
 while [ "$thedate" != "$start_release" ]
 do
 # 	echo $thedate
-	IDATE2=$(date -u -d "$end -$i hours" +%Y%m%d)
-	ITIME2=$(date -u -d "$end -$i hours" +%H%M%S)
-	
-# 	echo $IDATE1
-# 	echo $ITIME1
-	
-	i=$(( i - $step ))
-	
 	IDATE1=$(date -u -d "$end -$i hours" +%Y%m%d)
 	ITIME1=$(date -u -d "$end -$i hours" +%H%M%S)
-	
-# 	echo $IDATE2
-# 	echo $ITIME2
+
+	IDATE2=$(date -u -d "$end -$i hours" +%Y%m%d)
+	ITIME2=$(date -u -d "$end -$i hours" +%H%M%S)
+
+	# Release end date, same as IDATE1
+	i=$(( i - $step ))
+
  	thedate=$(date -u -d "$end -$i hours")
 	
 cat <<EOM >>${WORK_PATH}/options/RELEASES
@@ -150,7 +130,7 @@ Z2      =         $z, ! Upper height of release box meters/hPa above reference l
 ZKIND   =              1, ! Reference level 1=above ground, 2=above sea level, 3 for pressure in hPa
 MASS    =       1.0000E0, ! Total mass emitted, only relevant for fwd simulations
 PARTS   =          $particles, ! Total number of particles to be released
-COMMENT =    "RELEASE 1 $IDATE1:$ITIME1", ! Comment, written in the outputfile
+COMMENT =    "RELEASE $releaseno $IDATE1:$ITIME1", ! Comment, written in the outputfile
 /
 EOM
 	
@@ -185,27 +165,38 @@ cat <<EOM >${WORK_PATH}/options/COMMAND
  LOUTSTEP=           $OUTSTEP, ! Interval of model output; average concentrations calculated every LOUTSTEP (s)  
  LOUTAVER=           $OUTSTEP, ! Interval of output averaging (s)
  LOUTSAMPLE=          900, ! Interval of output sampling  (s), higher stat. accuracy with shorter intervals
- ITSPLIT=        99999999, ! Interval of particle splitting (s) 
+ LOUTRESTART=       -1, ! Interval of writing restart files (s), switched off when set to -1
+ LRECOUTSTEP=        3600, ! Interval of model output at receptors (s)
+ LRECOUTAVER=        3600, ! Interval of receptor output averaging (s)
+ LRECOUTSAMPLE=      1200, ! Interval of receptor output sampling (s)
  LSYNCTIME=           900, ! All processes are synchronized to this time interval (s)
  CTL=          -5.0000000, ! CTL>1, ABL time step = (Lagrangian timescale (TL))/CTL, uses LSYNCTIME if CTL<0
  IFINE=                 4, ! Reduction for time step in vertical transport, used only if CTL>1 
- IOUT=                  13, ! Output type: [1]mass 2]pptv 3]1&2 4]plume 5]1&4, +8 for NetCDF output     
- IPOUT=                 1, ! Particle position output: 0]no 1]every output 2]only at end 3]time averaged
+ IOUT=                  13, ! Gridded output type: [0]off [1]mass 2]pptv 3]1&2 4]plume 5]1&4, +8 for NetCDF output     
+ IPOUT=                 1, ! Particle position output: 0]off 1]every output 2]only at end
  LSUBGRID=              1, ! Increase of ABL heights due to sub-grid scale orographic variations;[0]off 1]on 
  LCONVECTION=           1, ! Switch for convection parameterization;0]off [1]on
+ LTURBULENCE=           1, ! Switch for turbulence parameterisation;0]off [1]on
+ LTURBULENCE_MESO=      0, ! Switch for mesoscale turbulence parameterisation;0]off (recommended) [1]on
  LAGESPECTRA=           1, ! Switch for calculation of age spectra (needs AGECLASSES);[0]off 1]on
- IPIN=                  0, ! Warm start from particle dump (needs previous partposit_end file); [0]no 1]yes  
+ IPIN=                  0, ! Warm start from particle dump; [0]no 1]from restart.bin file 2]from previous partoutput file 3]self made initial conditions 4]restart.bin and self made initial conditions
  IOUTPUTFOREACHRELEASE= 1, ! Separate output fields for each location in the RELEASE file; [0]no 1]yes 
  IFLUX=                 0, ! Output of mass fluxes through output grid box boundaries
  MDOMAINFILL=           0, ! Switch for domain-filling, if limited-area particles generated at boundary
- IND_SOURCE=            1, ! Unit to be used at the source   ;  [1]mass 2]mass mixing ratio 
- IND_RECEPTOR=          2, ! Unit to be used at the receptor; [1]mass 2]mass mixing ratio 3]wet depo. 4]dry depo.
+ IND_SOURCE=            1, ! Unit to be used at the source; [1]mass 2]mass mixing ratio 
+ IND_RECEPTOR=          2, ! Unit to be used at the receptor; [0]no receptor [1]mass 2]mass mixing ratio 3]wet depo. 4]dry depo.
  MQUASILAG=             0, ! Quasi-Lagrangian mode to track individual numbered particles 
  NESTED_OUTPUT=         1, ! Output also for a nested domain 
+ LNETCDFOUT=            1, ! Gridded netcdf output: [0]no [1]yes
  LINIT_COND=            0, ! Output sensitivity to initial conditions (bkw mode only) [0]off 1]conc 2]mmr 
- SURF_ONLY=             0, ! Output only for the lowest model layer, used w/ LINIT_COND=1 or 2
+ SFC_ONLY=              0, ! Output only for the lowest model layer, used w/ LINIT_COND=1 or 2
  CBLFLAG=               0, ! Skewed, not Gaussian turbulence in the convective ABL, need large CTL and IFINE
  OHFIELDS_PATH= "../../flexin/", ! Default path for OH file
+ NXSHIFT=             359, ! Shift of the global meteorological data. Default 359 for ECMWF and 0 for GFS if not given
+ MAXTHREADGRID=         1, ! Set maximum number of threads for doing grid computations. Recommended to set this no higher than 16. High numbers create more overhead and a larger memory footprint, 1=no parallelisation on grid.
+ MAXFILESIZE=       10000, ! Maximum output of each partoutput NetCDF-4 file in Mb before a new one is created
+ LOGVERTINTERP=         0, ! Flag to set all vertical interpolation to logarithmic instead of linear
+ LCMOUTPUT=             0, ! Switch for the Linear Chemistry Module; [0] off [1] on
  /
 EOM
 
@@ -231,7 +222,7 @@ cat <<EOM >${WORK_PATH}/options/OUTGRID
 ! OUTHEIGHTS = HEIGHT OF LEVELS (UPPER BOUNDARY)                               *
 !*******************************************************************************
 &OUTGRID
- OUTLON0=    -179.00,
+ OUTLON0=    -180.00,
  OUTLAT0=     -90.00,
  NUMXGRID=       360,
  NUMYGRID=       180,
